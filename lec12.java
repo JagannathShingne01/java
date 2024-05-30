4099d57931c4093d53152e1e6757838a642ae226
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-05-30 18:49:09 +0530

    String

--- lec12.java
+++ lec12.java
@@ -16,4 +16,25 @@ class HelloWorld {
     }
 }
 
-
+// input a string from the user. Create a new string called ‘result’ in which you will replace the letter ‘e’ in the original string with letter ‘i’. 
+// Example : 
+// original = “eabcdef’ ; result = “iabcdif”
+// Original = “xyz” ; result = “xyz”
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("String:");
+      String name = sc.next();
+       String result = "";
+      for(int i=0;i<name.length();i++){
+          if(name.charAt(i) == 'e'){
+              result += "i";
+          }else{
+              result += name.charAt(i);
+          }
+      }
+      System.out.println(result);
+   
+    }
+}
