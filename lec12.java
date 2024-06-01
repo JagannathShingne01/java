5aa8edbf2581863ea231751bd0e4098cd7d74062
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-06-01 19:44:28 +0530

    StringBuilder

--- lec12.java
+++ lec12.java
@@ -38,6 +38,22 @@ class HelloWorld {
    
     }
 }
+// same question using String Builder method lec-13
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String js = sc.next();
+       StringBuilder sb = new StringBuilder(js);
+       for(int i=0;i<sb.length();i++){
+           if(sb.charAt(i)=='e'){
+               sb.setCharAt(i, 'i');
+           }
+       }
+       System.out.println(sb);
+       
+    }
+}
 
 
 // 3)Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
