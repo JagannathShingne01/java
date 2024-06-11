98c9576d758e880db012109f33271806e61531c1
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-06-11 18:22:27 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -14,3 +14,19 @@ public static void towerOfHanoi(int n, String src, String helper, String dest){
     towerOfHanoi(n, "S","H","D");
     }
 }
+
+// Print a String in reverse
+class Recursion {
+    public static void func(String word, int idx){
+        if(idx == 0){
+            System.out.print(word.charAt(idx));
+            return;
+        }
+         System.out.print(word.charAt(idx));
+        func(word, idx-1);
+    }
+    public static void main(String[] args) {
+    String word = "abcs";
+    func(word, word.length()-1);
+    }
+}
