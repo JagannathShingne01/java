74d4cb35ee0430b7c17385bb2921622c1fad0ca7
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-06-21 20:29:54 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -34,3 +34,43 @@ class HelloWorld {
         System.out.println(search(str,target));
     }
 }
+
+// min NUM in array
+class HelloWorld {
+     public static int leFun(int arr[]){
+        int minNum = arr[0]; 
+        for(int idx = 0;idx<arr.length;idx++){
+            if(minNum>arr[idx]){
+                minNum = arr[idx];
+            }
+        }
+        System.out.println(minNum);
+        return -1;
+     }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,7,5,6,-7};
+     
+        int tp = leFun(arr);
+       
+    }
+}
+ //max NUM in 2D array
+class HelloWorld {
+     public static int leFun(int arr[][]){
+        int maxNum = arr[0][0]; 
+        for(int i = 0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(maxNum<arr[i][j]){
+                maxNum = arr[i][j];
+            }
+            }
+        }
+        System.out.println(maxNum);
+        return -1;
+     }
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3,-1},{3,2,3,0,10}};
+        int tp = leFun(arr);
+       
+    }
+}
