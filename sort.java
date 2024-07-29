1d188c3d24d5529be332a3653288fa2479213e5d
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-07-29 20:23:01 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -74,3 +74,27 @@ class Solution {
         return new int[] {-1, -1};
     }
 }
+
+//41 First missing Positive
+//https://leetcode.com/problems/first-missing-positive/
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            int c = nums[i] -1;
+            if(nums[i]>0 && nums[i]<=nums.length && nums[i] != nums[c]){
+                int temp = nums[i];
+                nums[i] = nums[c];
+                nums[c] = temp;
+            }else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != j+1){
+                return j+1;
+            }
+        }
+        return nums.length+1;
+    }
+}
